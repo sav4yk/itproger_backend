3cc07475f1732f95b66d0c8927c767225d964048
Sav4yk <mail@sav4yk.ru>
2020-01-30 23:30:52 +0300

    Выборка значений из базы данных

--- lesson3/index.php
+++ lesson3/index.php
@@ -7,14 +7,42 @@
 	<body>
 		<?php
 				$user = 'root';
-				$password = 'root';
+				$password = '';
 				$db = 'testing';
 				$host = 'localhost';
 
 				$dsn = 'mysql:host='.$host.';dbname='.$db;
 				$pdo = new PDO($dsn, $user, $password);
 
-				
+				$query = $pdo -> query('SELECT * FROM `users` ORDER BY `id` DESC LIMIT 2');
+				while($row = $query->fetch(PDO::FETCH_ASSOC)) { // вывод как массив
+					echo '<h1>'.$row['login'].'</h1><p><b>Email:' . $row['email']. '</b></p>';
+				}
+
+				$query = $pdo -> query('SELECT * FROM `users`');
+				while($row = $query->fetch(PDO::FETCH_OBJ)) { // вывод как объект
+					echo '<h1>'.$row->login.'</h1><p><b>Email:' . $row->email. '</b></p>';
+				}
+
+				$name = 'Петр';
+				$sql = 'SELECT `name`, `id`, `email` FROM `users` WHERE `name` = ?';
+				$query = $pdo->prepare($sql);
+				$query->execute([$name]); //используя массив
+				$users = $query->FetchAll(PDO::FETCH_OBJ);
+				var_dump($users);
+				foreach($users as $user) {
+					echo $user->email.'<br>';
+				}
+
+				$name = 'Петр';
+				$sql = 'SELECT `name`, `id`, `email` FROM `users` WHERE `name` = :name';
+				$query = $pdo->prepare($sql);
+				$query->execute(['name' => $name]); //используя ассоциативный массив
+				$users = $query->FetchAll(PDO::FETCH_OBJ);
+				var_dump($users);
+				foreach($users as $user) {
+					echo $user->email.'<br>';
+				}
 		?>
 	</body>
 </html>
