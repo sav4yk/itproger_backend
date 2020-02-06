1e049bf9d03a6a0d217da62e7f54d07dd3df79cf
Sav4yk <mail@sav4yk.ru>
2020-02-06 20:22:32 +0300

    Вывод всех статей из базы данных

--- lesson4/index.php
+++ lesson4/index.php
@@ -4,7 +4,19 @@ $website_title="PHP BLOG";
 <main class="container mt-5">
 	<div class="row">
 		<div class="col-md-8 mb-3">
-			Основная часть сайта
+			<?php
+			require_once 'mysql_connect.php';
+
+			$sql = 'SELECT * FROM `articles` ORDER BY `date` DESC';
+			$query = $pdo->query($sql);
+			while ($row = $query->fetch(PDO::FETCH_OBJ)) {
+				echo "<h2>$row->title</h2>
+				<p>$row->intro</p>
+				<p><b>Автор статьи:</b><mark>$row->avtor</mark></p>
+				<button class='brn btn-warning mb-5'>Прочитать больше</button>";
+			}
+
+			 ?>
 		</div>
 <?php require('blocks/aside.php'); ?>
 	</div>
