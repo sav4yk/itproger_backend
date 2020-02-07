86aec9183e5b34c866c74b3dd140e6c59610b49b
Sav4yk <mail@sav4yk.ru>
2020-02-07 16:32:07 +0300

    Форма добавление комментариев к статьям

--- lesson4/news.php
+++ lesson4/news.php
@@ -28,6 +28,36 @@ $website_title=$article->title . " | PHP BLOG";
 					<?=$article->text?>
 				</p>
 			</div>
+			<h3 class="mt-5">Коментарий</h3>
+			<form action="news.php?id=<?=$_GET['id']?>" method="post">
+				<label for="username">Ваше имя</label>
+				<input type="text" name="username" id="username" value="<?=$_COOKIE['log']?>" class="form-control">
+				<label for="mess">Сообщение</label>
+				<textarea name="mess" id="mess" class="form-control"></textarea>
+				<button type="submit" class="btn btn-success mt-3 mb-5" id="mess_send" name="mess_send">Добавить комментарий</button>
+			</form>
+			<?php
+			if (isset($_POST['username']) && isset($_POST['mess']) && $_POST['username']!='' && $_POST['mess']!='') {
+				$username = trim(filter_var($_POST['username'], FILTER_SANITIZE_STRING));
+				$mess = trim(filter_var($_POST['mess'], FILTER_SANITIZE_STRING));
+				$sql = 'INSERT INTO comments(name, mess, article_id) VALUES(?, ?, ?)';
+				$query = $pdo->prepare($sql);
+				$query->execute([$username, $mess, $_GET['id']]);
+			}
+
+
+			$sql='SELECT * FROM `comments` WHERE article_id=:id ORDER BY id DESC';
+			$query = $pdo->prepare($sql);
+			$query->execute(['id' => $_GET['id']]);
+			$comments = $query->fetchAll(PDO::FETCH_OBJ);
+			foreach ($comments as $comment) {
+				echo "<div class='alert alert-info mb-2'>
+				<h4>$comment->name</h4>
+				<p>$comment->mess</p>
+				</div>";
+				// code...
+			}
+			 ?>
 		</div>
 <?php require('blocks/aside.php'); ?>
 	</div>
