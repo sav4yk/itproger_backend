40c6b80933251302b37a1a2b28f60310bed76088
Vladimir Savchuk <reg@sav4yk.ru>
2020-02-12 19:00:01 +0300

    PSR стандарты

---
+++ lesson8/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            require 'autoload.php';
+
+            $bob = new User();
+            $bob->show();
+
+         ?>
+    </body>
+</html>
