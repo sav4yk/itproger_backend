296e284704c5f55b83c1c7764b5f97e75996981a
sav4yk <mail@sav4yk.ru>
2020-03-24 23:54:44 +0300

    Первичная разметка главной страницы

--- diplom/app/views/home/index.tpl.php
+++ diplom/app/views/home/index.tpl.php
@@ -1,2 +1,83 @@
-<?php
-echo 'Hi';
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Сокрытить ссылку быстро и качественно">
+    <title>Сокращатель | Sav4yk.ru</title>
+    <link href="/public/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Favicons -->
+    <link rel="icon" href="/img/favicon.ico">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+    <link href="/public/css/short.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+    <header class="masthead mb-auto">
+
+        <div class="inner">
+
+                <h3 class="masthead-brand">Сокращатель</h3>
+
+
+            <nav class="nav nav-masthead justify-content-center">
+                <a class="nav-link active" href="#">Главная</a>
+                <a class="nav-link" href="#">Про нас</a>
+                <a class="nav-link" href="#">Контакты</a>
+                <a class="nav-link" href="#">Войти</a>
+            </nav>
+        </div>
+    </header>
+
+    <main role="main" class="inner cover">
+        <a class="navbar-brand" href="#">
+            <img src="/public/img/emblem.png" class="" alt="">
+        </a>
+        <h1 class="cover-heading">Сократи длинную ссылку.</h1>
+        <p class="lead">Ваших друзей пугают длинные ссылку, занимающие много места в комментариях? Сократи ссылки с помощью нашего сервиса!</p>
+        <form>
+            <div class="form-row align-items-center">
+                <div class="col-auto">
+                    <input type="text" class="form-control mb-2" id="inlineFormInput" placeholder="Введите email">
+                </div>
+                <div class="col-auto">
+                    <input type="email" class="form-control mb-2" id="inlineFormInput" placeholder="Введите login">
+                </div>
+                <div class="col-auto">
+                    <input type="password" class="form-control mb-2" id="inlineFormInput" placeholder="Введите пароль">
+                </div>
+            </div>
+            <div class="form-row align-items-center">
+                <div class="col-auto">
+                    <button type="button" class="btn btn-secondary mb-2">Зарегистрируйся</button>
+                </div>
+            </div>
+        </form>
+
+    </main>
+
+    <footer class="mastfoot mt-auto">
+        <div class="inner">
+            <p>Создан в 2020 году | <a href="http://sav4yk.ru/">Sav4yk.ru</a></p>
+        </div>
+    </footer>
+</div>
+</body>
+</html>
